335030f8d260c3e9fa905c9f69731b4e97f76fd5
ninja3571 <125036855+ninja3571@users.noreply.github.com>
2023-04-28 16:00:36 +0200

    28.04

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 map.on('click', function(e){
     console.log(e.latlng)
 
-    addMarker(e.latlng)
+    addMarker(e)
 })
 
 var geojsonFeature = {
@@ -23,32 +23,108 @@ var geojsonFeature = {
     },
     "geometry": {
         "type": "Point",
-        "coordinates": [52.186, 21.571]
+        "coordinates": [21.571, 52.186]
     }
 };
 
 L.geoJSON(geojsonFeature).addTo(map);
+//modyfikowany znacznik
+var greenIcon = L.icon({
+    iconUrl: 'https://cdn-icons-png.flaticon.com/512/141/141803.png',
 
-var myLines = [{
-    "type": "LineString",
-    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-}, {
-    "type": "LineString",
-    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
-}];
+    iconSize:     [40, 40], // size of the icon
+    iconAnchor:   [20, 40], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
-function addMarker(latlng){
-    L.marker(latlng).addTo(map)
+L.marker([52.186, 21.571], {icon: greenIcon}).addTo(map);
+
+//znaczniki po kliknięciu
+function addMarker(e){
+    L.marker([e.latlng.lat,e.latlng.lng]).addTo(map)
+
+    const lat = e.latlng.lat
+    const lng = e.latlng.lng
+    
+    var myLines = [{
+        "type": "LineString",
+        "coordinates": [[21.571, 52.186], [lng, lat]]
+    }, 
+]
+
+
+
+//ustawienia znaczników(kolor, szerokość)
+    var myStyle = {
+        "color": "#ff00ff",
+        "weight": 4,
+        "opacity": 0.65
+    };
+
+    L.geoJSON(myLines, {
+        style: myStyle
+    }).addTo(map);
 }
 
-/*async function getData(latlng){
+async function getData(latlng){
 
     const lng = latlng.lng
     const lat = latlng.lat
 
     console.log(lng)
 
-    const url =await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}`)
+    const url =await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&namedetails=1`)
+
     const data = await fetch(url)
     const json= await data.json
-}*/
+
+    console.log(json.features[0].properties.addres.county)
+}
+
+
+
+//napis "Mińsk Mazowiecki" nad danym miejscem
+var popup = L.popup()
+.setLatLng([52.186, 21.571])
+.setContent("Mińsk Mazowiecki")
+.openOn(map);
+
+
+for(let i=0;1<=powiaty.features.length-1;i++){
+    var powiat = powiaty.features
+    var mappowiat = L.geoJSON(powiat).addTo(map)
+    mappowiat.blindTooltip(powiaty.features[i].properties.nazwa)
+}
+console.log(powiaty.features)
+
+/*function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    layer.bringToFront();
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+
+var geojson;
+// ... our listeners
+geojson = L.geoJson();
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+    });
+}
+
+geojson = L.geoJson(statesData, {
+    onEachFeature: onEachFeature
+}).addTo(map);*/
